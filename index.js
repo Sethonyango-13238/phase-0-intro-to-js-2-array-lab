153f808a126d34b6028e99fd490715ce46b6a7bb
Sethonyango-13238 <onyangosethoyugi@gmail.com>
2024-07-12 00:16:42 +0300

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,64 @@
 // Write your solution here!
+const cats=["Milo", "Otis", "Garfield"];
+// beforeEach(function () {
+//     cats.length = 0;
+
+//     cats.push('Milo', 'Otis', 'Garfield');
+//   });
+console.log(cats)
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+destructivelyAppendCat("Ralph");
+console.log(cats);
+
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+}
+destructivelyPrependCat("BOb");
+console.log(cats);
+
+function destructivelyRemoveLastCat(name){
+    cats.pop(name)
+}
+destructivelyRemoveLastCat("Ralph")
+console.log(cats)
+
+function destructivelyRemoveFirstCat(name){
+    cats.shift(name)
+}
+destructivelyRemoveFirstCat("Bob");
+console.log(cats);
+
+function appendCat(cats, name){
+    return [...cats,name];
+}
+const appendCats=appendCat(cats, "Broom")
+console.log(cats);
+console.log(appendCats);
+
+function prependCat(cats, name){
+    return [name, ...cats];
+}
+const prependCats=prependCat(cats, "Arnold")
+console.log(cats);
+console.log(prependCats);
+
+function removeLastElement(arr) {
+    return [...arr.slice(0, arr.length - 1)];
+}
+
+const newCats = removeLastElement(cats);
+
+console.log(cats);      
+console.log(newCats); 
+
+function removeFirstElement(arr) {
+    return cats.slice(1);
+}
+
+const newCatss = removeLastElement(cats);
+
+console.log(cats);      
+console.log(newCatss); 
